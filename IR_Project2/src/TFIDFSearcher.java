34db1e19bcb820af7c238b7593f7d5760175077e
Nontapat Pintira <31785907+Mr-JNP@users.noreply.github.com>
2019-10-26 12:16:23 +0700

    Add comments to TFIDFSearcher

--- IR_Project2/src/TFIDFSearcher.java
+++ IR_Project2/src/TFIDFSearcher.java
@@ -12,6 +12,13 @@ import java.util.Set;
 
 public class TFIDFSearcher extends Searcher
 {	
+	/*
+	 * double norm This is a tempory variable to store the norm of a document vector.
+	 * Map<Integer, Map<String, Double>> tfidf This is a map which maps document id to document vector.
+	 * Map<String, Integer> dfs This is a map which maps a term to its document frequency.
+	 * Map<String, Double> idfs This is a map which maps a term to its inverted document frequency.
+	 * Map<Integer, Double> normds This is a map which maps a document id to its norm.
+	 */
 	double norm;
 	Map<Integer, Map<String, Double>> tfidf = new HashMap<>();
 	Map<String, Integer> dfs = new HashMap<>();
@@ -21,34 +28,39 @@ public class TFIDFSearcher extends Searcher
 	public TFIDFSearcher(String docFilename) {
 		super(docFilename); 
 		/************* YOUR CODE HERE ******************/
-		
-//		Loop to count term frequency, document frequency.
+		/*
+		 * Loop to count term frequency and document frequency
+		 * 1. Create set to keep unique terms
+		 * 2. Iterate through each term to count document frequency and put it in a map
+		 * 	2.1. If the term already exist then increment the df of the given term
+		 * 	2.2. Add new term to the map otherwise.
+		 * 3. For each term in the document, count term frequency and put it in another map
+		 */
 		for(Document d: this.documents) {
 			Set<String> terms = new HashSet<String>(d.getTokens());
 			Map<String, Double> tfs = new HashMap<>();
 			for(String term: terms) {
-				
-//				Count document frequency
-//				If the term already exist then increment
-//				Add the the term to the map otherwise
-				if (dfs.get(term) != null)	dfs.put(term, dfs.get(term) + 1);
-				else						dfs.put(term, 1);
-				
+				if (dfs.get(term) != null)
+					dfs.put(term, dfs.get(term) + 1);
+				else
+					dfs.put(term, 1);
 				tfs.put(term, tf(d.getTokens(), term));
 			}
 			tfidf.put(d.getId(), tfs);
 		}
-		
-//		Loop to calculate inverted document frequency
+		/*
+		 * For each term, calculate inverted document frequency
+		 */
 		for(String term: dfs.keySet()) {
 			idfs.put(term, Math.log10(1 + ((double) this.documents.size() / dfs.get(term))));
 		}
-		
-//		Loop to calculate td-idf weight
+		/*
+		 * 1. For each document, calculate tfidf weight. (As a vector)
+		 * 2. Calculate the norm of each vector, and store it in a map.
+		 */
 		for(Integer i: tfidf.keySet()) {
 			norm = 0;
 			for(String term: tfidf.get(i).keySet()) {
-//				System.out.println(i + ": " + term + ", " + tfidf.get(i).get(term) + ", " + idfs.get(term) + ", " + tfidf.get(i).get(term) * idfs.get(term));
 				tfidf.get(i).put(term, tfidf.get(i).get(term) * idfs.get(term));
 				norm += Math.pow(tfidf.get(i).get(term), 2);
 			}
@@ -57,7 +69,12 @@ public class TFIDFSearcher extends Searcher
 		/***********************************************/
 	}
 	
-	
+	/*
+	 * Calculate term frequency
+	 * @param d This is the list of token in a document.
+	 * @param t This is the term that we want to check for its frequency.
+	 * @return double This returns the term frequency (with logarithmic function).
+	 */
 	public double tf(List<String> d, String t) {
 		int f = 0;
 		for(String s: d)
@@ -66,46 +83,53 @@ public class TFIDFSearcher extends Searcher
 		else		return 1 + Math.log10(f);
 	}
 	
+	/*
+	 * Calculate cosine similarity score
+	 * 1. Calculate the norm of query vector and document vector>
+	 * 2. Calculate the dot product
+	 * @param query This is the query vector.
+	 * @param docId This is the document id of the document we are comparing.
+	 * @return double This returns the cosine similarity of the query and the document.
+	 */
 	public double cosine(Map<String, Double> query, int docId) {
-		
 		double dot = 0, normq = 0, normd;
 		HashSet<String> intersection = new HashSet<>(query.keySet());
-//		Find norm of the query vector
-		for(String term: query.keySet()) {
-			normq += Math.pow(query.get(term), 2);
-		}
-		normq = Math.sqrt(normq);
 		
-//		Get norm of document with document id
+		for(String term: query.keySet())
+			normq += Math.pow(query.get(term), 2);
+		normq = Math.sqrt(normq);
 		normd = normds.get(docId);
 		
-//		Find dot product
 		intersection.retainAll(tfidf.get(docId).keySet());
-		for(String term: intersection) {
+		for(String term: intersection)
 			dot += query.get(term) * tfidf.get(docId).get(term);
-		}
 		
-//		System.out.println(dot + " " + normq + " " + normd);
 		return dot/(normq*normd);
 	}
 	
-	
+	/*
+	 * Get top K result using cosine similarity score
+	 * @param queryString This is the query.
+	 * @param k The number of result that the system returns.
+	 * @return List<SearchResult> This return a list of search result type of top k result.
+	 * 
+	 * 1. Construct a query vector
+	 * 2. Compute cosine similarity score
+	 * 3. Sort the list
+	 * 4. Get top K result
+	 */
 	@Override
 	public List<SearchResult> search(String queryString, int k) {
 		/************* YOUR CODE HERE ******************/
 		List<SearchResult> cosineScore = new ArrayList<>();
 		List<SearchResult> results = new ArrayList<>();
 		Map<String, Double> qVector = new HashMap<>();
-		
-//		Create set of unique term for query
 		Set<String> qTerm = new HashSet<>(tokenize(queryString));
 		
-//		Create the vector of the query
 		for(String term: qTerm) {
 			qVector.put(term, tf(tokenize(queryString), term) * idfs.get(term));
 		}
 		
-//		Calculate cosine similarity
 		for(Document d: this.documents) {
 			cosineScore.add(new SearchResult(d, cosine(qVector, d.getId())));
 		}
